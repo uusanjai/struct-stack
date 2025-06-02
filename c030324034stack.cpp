f02c2cf26fcd37202560a611c023aadf3da828a3
uusanjai <c030324034@mahasiswa.poliban.ac.id>
2025-06-03 01:56:54 +0800

    fungsi push & validasi penuh

--- c030324034stack.cpp
+++ c030324034stack.cpp
@@ -19,9 +19,21 @@ int full(Stack* S) {
 int empty(Stack* S) {
     return S->count == 0;
 }
+void push (itemtype x, stack* s) {
+    if (full(s))
+    cout << "Stack penuh!! Tidak dapat menambahkan, harap ambil terlebih dahulu baru masukkan " << x << ".\n";
+} else {
+    s->item[s->count] - x;
+    s->count++;
+    cout << "push" << x << " berhasil dimasukkan.\n"
+ }
+
 int main() {
     Stack nilai;
     InitializeStack(&nilai);
+
+    push(20, &nilai);
+    push(90, &nilai);
 
     return 0;
 }
