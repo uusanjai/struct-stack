99a0a26b5665ad596d3087fc7057f58211c88040
uusanjai <c030324034@mahasiswa.poliban.ac.id>
2025-06-03 02:20:53 +0800

    fungsi pop dengan validasi kosong

--- c030324034stack.cpp
+++ c030324034stack.cpp
@@ -9,9 +9,9 @@ typedef struct {
     int count;
 } Stack;
 
-void initializeStack(Stack* S) {
-    s->count = 0;
-    cout <<"stack telah di inisialiisasi.\n";
+void InitializeStack(Stack* S) {
+    S->count = 0;
+    cout <<"stack telah diinisialiisasi.\n";
 }
 int full(Stack* S) {
     return S->count == MAXSTACK;
@@ -19,21 +19,41 @@ int full(Stack* S) {
 int empty(Stack* S) {
     return S->count == 0;
 }
-void push (itemtype x, stack* s) {
-    if (full(s))
-    cout << "Stack penuh!! Tidak dapat menambahkan, harap ambil terlebih dahulu baru masukkan " << x << ".\n";
-} else {
-    s->item[s->count] - x;
-    s->count++;
-    cout << "push" << x << " berhasil dimasukkan.\n"
- }
+
+void push (ItemType x, Stack* s) {
+    if (full(s)) {
+        cout << "Stack penuh!! Tidak dapat menambahkan, harap ambil terlebih dahulu baru masukkan " << x << ".\n";
+    } else {
+        s->item[s->count] = x;
+        s->count++;
+        cout << "push: " << x << " berhasil dimasukkan.\n";
+    }
+}
+
+
+void pop(Stack* S, ItemType* X) {
+    if (empty(S)) {
+        cout << "Stack kosong! Tidak dapat melakukan pop.\n";
+    } else {
+        S->count--;
+        *X = S->item[S->count];
+        cout << "Pop: " << *X << " berhasil diambil.\n";
+    }
+}
+
 
 int main() {
     Stack nilai;
+    ItemType x;
     InitializeStack(&nilai);
 
     push(20, &nilai);
     push(90, &nilai);
+    push(10, &nilai);
+    push(20, &nilai);
+
+    pop(&nilai, &x);
+
 
     return 0;
 }
